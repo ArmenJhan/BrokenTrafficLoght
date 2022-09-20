046a02f77cbb0cb7e7a5655a63d832519d0fcf25
Armen <madarmen1987@gmail.com>
2022-09-20 18:23:00 +0300

    Initial Commit

---
+++ BrokenTrafficLoght/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrokenTrafficLoght
+//
+//  Created by Armen Madoyan on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
